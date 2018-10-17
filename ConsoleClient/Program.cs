cc7953b5b0b427c731d10baa4f88d14843471408
Mikael <mikaelkarlgren@hotmail.se>
2018-10-17 10:02:23 +0200

    Console app base

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleClient
 {
@@ -6,7 +7,33 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] commands = new string[] { "quit" };
+            while(true)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("Commands:");
+                foreach(string s in commands)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                string input = Console.ReadLine();
+                if (commands.Any(c => c == input))
+                {
+                    switch(input) {
+                        case "quit":
+                            return;
+                        default:
+                            Console.WriteLine($"There's no current support for command \"{input}\"");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"\"{input}\" is not supported, use an alternative from the command list");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
