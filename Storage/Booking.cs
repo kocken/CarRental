168ad7e3b18bf615dbf3daf7484d2fa730932a7b
Marinel <info@mdshop.se>
2018-10-17 10:16:49 +0200

    new classes

---
+++ Storage/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage
+{
+    public class Booking
+    {
+        public Car Car { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
---
+++ Storage/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Storage
+{
+    public class Car
+    {
+        public string RegistrationNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
